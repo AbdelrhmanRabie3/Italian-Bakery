868d2042de4bbbfa319acd06bb51cfa7819aba48
AbdelrhmanRabie3 <abdelrhmanrabie33@gmail.com>
2025-05-19 00:08:49 +0300

    fixing mobile responsivity

--- js/index.js
+++ js/index.js
@@ -1,30 +1,32 @@
+AOS.init({
+  duration: 900,
+  mirror: true,
+  once: false,
+  offset: 50,
+  anchorPlacement: 'top-center',
+  disable: window.innerWidth < 768 ? 'mobile' : false
+});
 
-  AOS.init({
-    duration: 900,
-    mirror: true,
-    once: false
-  });
+// Scrollspy
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: '#mainNav',
+  offset: 60
+});
 
-  // Scrollspy
-  var scrollSpy = new bootstrap.ScrollSpy(document.body, {
-    target: '#mainNav',
-    offset: 80
+// Collapse navbar on link click (mobile)
+document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
+  link.addEventListener('click', function(){
+    var nav = document.querySelector('.navbar-collapse');
+    if(nav.classList.contains('show')){
+      var bsCollapse = new bootstrap.Collapse(nav, {toggle: false});
+      bsCollapse.hide();
+    }
   });
+});
 
-  // Collapse navbar on link click (mobile)
-  document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
-    link.addEventListener('click', function(){
-      var nav = document.querySelector('.navbar-collapse');
-      if(nav.classList.contains('show')){
-        var bsCollapse = new bootstrap.Collapse(nav, {toggle: false});
-        bsCollapse.hide();
-      }
-    });
-  });
-
-  // Contact form fake submit
-  document.querySelector('.contact-form').addEventListener('submit', function(e){
-    e.preventDefault();
-    alert('Thank you for contacting us! We will get back to you soon.');
-    this.reset();
-  });
+// Contact form fake submit
+document.querySelector('.contact-form').addEventListener('submit', function(e){
+  e.preventDefault();
+  alert('Thank you for contacting us! We will get back to you soon.');
+  this.reset();
+});
